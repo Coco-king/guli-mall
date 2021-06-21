0e9f7beed7f647ecf072b0f15df0b43095275a1b
codecrab <3060550682@qq.com>
2021-06-21 08:28:41 +0800

    新增认证模块

---
+++ gulimall-auth/src/main/java/top/codecrab/gulimall/auth/GulimallAuthApplication.java
@@ -0,0 +1,13 @@
+package top.codecrab.gulimall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthApplication.class, args);
+    }
+
+}
